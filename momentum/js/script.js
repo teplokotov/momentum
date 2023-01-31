0014f5a45971877f4157830298b5d3db40dd1c87
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-01-31 13:18:37 +0300

    feat: add time function

---
+++ momentum/js/script.js
@@ -0,0 +1,18 @@
+const time = document.querySelector('.time');
+const Localedate = document.querySelector('.date');
+
+function showTime() {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+    time.textContent = currentTime;
+    showDate();
+    setTimeout(showTime, 1000);
+}
+showTime();
+
+function showDate() {
+    const date = new Date();
+    const options = {weekday: 'long', month: 'long', day: 'numeric'};
+    const currentDate = date.toLocaleDateString('ru-Ru', options);
+    Localedate.textContent = currentDate;
+}
