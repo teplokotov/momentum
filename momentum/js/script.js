d7aedacec7a2409ceba95c7e4420bc6d3e86f1fe
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-02-01 00:27:21 +0300

    feat: add image slider function

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,7 +1,12 @@
+const body = document.querySelector('body');
 const time = document.querySelector('.time');
 const Localedate = document.querySelector('.date');
 const greeting = document.querySelector('.greeting');
 const InputName = document.querySelector('.name');
+let randomNum;
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+
 
 function showTime() {
     const date = new Date();
@@ -30,22 +35,22 @@ function getTimeOfDay() {
     const currentHour = getHours();
     let greetingText;
     if (currentHour >= 6 && currentHour < 12) {
-        greetingText = 'Доброе утро,';
+        greetingText = 'morning';
     }
     if (currentHour >= 12 && currentHour < 18) {
-        greetingText = 'Добрый день,';
+        greetingText = 'afternoon';
     }
     if (currentHour >= 18 && currentHour < 24) {
-        greetingText = 'Добрый вечер,';
+        greetingText = 'evening';
     }
     if (currentHour >= 0 && currentHour < 6) {
-        greetingText = 'Доброй ночи,';
+        greetingText = 'night';
     }
     return greetingText;
 }
 
 function showGreeting() {
-    greeting.textContent = getTimeOfDay();
+    greeting.textContent = 'Good ' + getTimeOfDay() + ',';
 }
 
 function setLocalStorage() {
@@ -60,3 +65,42 @@ function getLocalStorage() {
     }
 }
 window.addEventListener('load', getLocalStorage);
+
+function getRandomNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRandomNum(1,20);
+
+function setBg() {
+    let timeOfDay = getTimeOfDay();
+    console.log(randomNum);
+    let bgNum = String(randomNum).padStart(2, "0");
+    const img = new Image();
+    img.src = "https://raw.githubusercontent.com/teplokotov/stage1-tasks/assets/images/" + timeOfDay + "/" + bgNum + ".jpg"; 
+    img.onload = () => {      
+        body.style.backgroundImage = "url('" + img.src + "')";
+    }; 
+}
+setBg();
+
+function getSlideNext() {
+    if (randomNum < 20) {
+        randomNum++;
+    } else {
+        randomNum = 1;
+    }
+    setBg();
+}
+slideNext.addEventListener('click', getSlideNext);
+
+function getSlidePrev() {
+    if (randomNum > 1) {
+        randomNum--;
+    } else {
+        randomNum = 20;
+    }
+    setBg();
+}
+slidePrev.addEventListener('click', getSlidePrev);
