1a529b0035d75775f81ab7bb31480d631be0e008
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-01-31 15:28:29 +0300

    feat: add greating function

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,11 +1,14 @@
 const time = document.querySelector('.time');
 const Localedate = document.querySelector('.date');
+const greeting = document.querySelector('.greeting');
+const InputName = document.querySelector('.name');
 
 function showTime() {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
     time.textContent = currentTime;
     showDate();
+    showGreeting();
     setTimeout(showTime, 1000);
 }
 showTime();
@@ -16,3 +19,44 @@ function showDate() {
     const currentDate = date.toLocaleDateString('ru-Ru', options);
     Localedate.textContent = currentDate;
 }
+
+function getHours() {
+    const date = new Date();
+    const hour = date.getHours();
+    return hour;
+}
+
+function getTimeOfDay() {
+    const currentHour = getHours();
+    let greetingText;
+    if (currentHour >= 6 && currentHour < 12) {
+        greetingText = 'Доброе утро,';
+    }
+    if (currentHour >= 12 && currentHour < 18) {
+        greetingText = 'Добрый день,';
+    }
+    if (currentHour >= 18 && currentHour < 24) {
+        greetingText = 'Добрый вечер,';
+    }
+    if (currentHour >= 0 && currentHour < 6) {
+        greetingText = 'Доброй ночи,';
+    }
+    return greetingText;
+}
+
+function showGreeting() {
+    greeting.textContent = getTimeOfDay();
+}
+
+function setLocalStorage() {
+    localStorage.setItem('name', InputName.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+    InputName.placeholder = '[Введите имя]';
+    if (localStorage.getItem('name')) {
+        InputName.value = localStorage.getItem('name');
+    }
+}
+window.addEventListener('load', getLocalStorage);
